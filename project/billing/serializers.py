efe2966a67594cba38e82e9caffdfe583e424151
1madatyou <khasanov.amir1323@gmail.com>
2023-11-29 18:23:56 +0700

    Create ItemSerializer and register Item model in django admin panel

---
+++ project/billing/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Item
+
+
+class ItemSerializer(ModelSerializer):
+
+    class Meta:
+        model = Item
+        fields = ["id", "name", "description", "price"]
