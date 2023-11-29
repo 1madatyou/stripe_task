164eee9b70a02223f1b05a151da459330a2365bb
1madatyou <khasanov.amir1323@gmail.com>
2023-11-29 18:39:12 +0700

    Create Order, Discount, Tax models and made migrations for them

--- project/billing/models.py
+++ project/billing/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 
 class Item(models.Model):
-
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Order(models.Model):
+    items = models.ManyToManyField(to=Item)
+
+class Discount(models.Model):
+    item = models.ForeignKey(to=Order, on_delete=models.CASCADE, related_name='discount')
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Tax(models.Model):
+    item = models.ForeignKey(to=Order, on_delete=models.CASCADE, related_name='tax')
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
