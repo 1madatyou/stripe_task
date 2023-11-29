c32b7349e05f2b3f8b2d1f3a2d7fd4952cd33ce0
1madatyou <khasanov.amir1323@gmail.com>
2023-11-29 17:41:33 +0700

    Add Stripe keys to settings.py

--- project/core/settings.py
+++ project/core/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+### Stripe ###
+
+STRIPE_PUBLISHABLE_KEY=os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY=os.getenv('STRIPE_SECRET_KEY')
