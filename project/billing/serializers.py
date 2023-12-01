8822990adad9ad5d29aa5216d70b6c7b7c5feeb1
1madatyou <khasanov.amir1323@gmail.com>
2023-12-01 14:47:20 +0700

    Create serializer for Order model

--- project/billing/serializers.py
+++ project/billing/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 
-from .models import Item
+from .models import Item, Order
 
 
 class ItemSerializer(ModelSerializer):
@@ -8,3 +8,9 @@ class ItemSerializer(ModelSerializer):
     class Meta:
         model = Item
         fields = ["id", "name", "description", "price"]
+
+class OrderSerializer(ModelSerializer):
+    items = PrimaryKeyRelatedField(queryset=Item.objects.all(), many=True)
+    class Meta:
+        model = Order
+        fields = ["items"]
